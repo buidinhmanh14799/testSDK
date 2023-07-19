0f659db169bca73a0d3b169c050ad55ccea29c0f
buidinhmanh <buidinhmanh14799@gmail.com>
2023-07-19 11:51:58 +0700

    fix

--- Sources/TestEdoctorSDK/TestEdoctorSDK.swift
+++ Sources/TestEdoctorSDK/TestEdoctorSDK.swift
@@ -2,8 +2,8 @@ import SwiftUI
 import WebKit
 
 public struct TestEdoctorSDK {
-    public static func openWebView(withURL url: String) -> some View {
-        WebViewCustom(url: url)
+    public static func openWebView() -> some View {
+        WebViewCustom()
     }
 }
 
