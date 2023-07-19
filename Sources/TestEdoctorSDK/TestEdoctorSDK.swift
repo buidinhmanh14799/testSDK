c43bf705ca77ecbc54be9570bf5fc36746d1beb5
buidinhmanh <buidinhmanh14799@gmail.com>
2023-07-19 12:43:19 +0700

    fix 2

--- Sources/TestEdoctorSDK/TestEdoctorSDK.swift
+++ Sources/TestEdoctorSDK/TestEdoctorSDK.swift
@@ -1,10 +1,26 @@
 import SwiftUI
 import WebKit
 
-public struct TestEdoctorSDK {
-    public static func openWebView() -> some View {
-        WebViewCustom()
+
+public class TestEdoctorSDK {
+    public static func openWebView(withURL urlString: String) {
+        guard let url = URL(string: urlString) else {
+            print("Invalid URL: \(urlString)")
+            return
+        }
+
+        let webView = WKWebView()
+        let viewController = UIViewController()
+        viewController.view = webView
+
+        let navigationController = UINavigationController(rootViewController: viewController)
+
+        if let topViewController = UIApplication.shared.keyWindow?.rootViewController {
+            topViewController.present(navigationController, animated: true) {
+                let request = URLRequest(url: url)
+                webView.load(request)
+            }
+        }
     }
 }
-
 
