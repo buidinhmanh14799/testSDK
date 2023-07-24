d44b58464dd9989b287878fab5e6b393c5e5ac75
buidinhmanh <buidinhmanh14799@gmail.com>
2023-07-24 16:37:00 +0700

    tt

--- Sources/TestEdoctorSDK/FullScreenWebview.swift
+++ Sources/TestEdoctorSDK/FullScreenWebview.swift
@@ -36,11 +36,11 @@ struct WebView: UIViewRepresentable {
     let urlString: String
     
     func makeUIView(context: Context) -> WKWebView {
-        let webView =
+        let webView = WKWebView()
         if let url = URL(string: urlString) {
-            CustomWebViewController(urlString: url)
+            CustomWebViewController(urlString: urlString)
         }
-        return null
+        return webView
     }
     
     func updateUIView(_ uiView: WKWebView, context: Context) {
