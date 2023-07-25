bb205fb56ab3cd5fdd74e5f16eeaea770acf8b30
buidinhmanh <buidinhmanh14799@gmail.com>
2023-07-25 10:23:21 +0700

    ok

--- Sources/TestEdoctorSDK/TestEdoctorSDK.swift
+++ Sources/TestEdoctorSDK/TestEdoctorSDK.swift
@@ -3,10 +3,7 @@ import WebKit
 
 
 public func openWebView(withURL urlString: String) {
-    guard let url = URL(string: urlString) else {
-        print("Invalid URL: \(urlString)")
-        return
-    }
+
 
     if let topViewController = UIApplication.shared.windows.first?.rootViewController {
             let customWebViewController = CustomWebViewController(urlString: urlString)
