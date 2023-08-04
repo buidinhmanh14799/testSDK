31683b27faaaadc7961594b36b3dfe351e475004
buidinhmanh <manhbd@edoctor.vn>
2023-08-04 14:39:00 +0700

    update

--- Sources/TestEdoctorSDK/TestEdoctorSDK.swift
+++ Sources/TestEdoctorSDK/TestEdoctorSDK.swift
@@ -2,6 +2,7 @@ import SwiftUI
 import WebKit
 
 
+
 public func openWebView(withURL urlString: String) {
 
 
---
+++ Sources/TestEdoctorSDK/call/CallManager.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bùi Đình Mạnh on 04/08/2023.
+//
+
+import Foundation
+import SendBirdCalls
+
+public func initSenbird () {
+    
+}
