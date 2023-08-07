29a7ca007acc8f53fc8e83c648dfdebe018898db
buidinhmanh <manhbd@edoctor.vn>
2023-08-07 10:13:51 +0700

    update code

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "testSDK",
+    name: "TestEdoctortéSDK",
     platforms: [
         .macOS(.v10_14), .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "testSDK",
-            targets: ["testSDK"]),
+            name: "TestEdoctorSDK",
+            targets: ["TestEdoctorSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
