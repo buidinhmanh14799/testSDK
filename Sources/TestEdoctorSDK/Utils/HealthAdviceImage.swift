6008d23b34ad6914382936af9d2f6d6819fc89c5
buidinhmanh <manhbd@edoctor.vn>
2023-08-25 14:34:27 +0700

    update code

--- Sources/TestEdoctorSDK/Utils/HealthAdviceImage.swift
+++ Sources/TestEdoctorSDK/Utils/HealthAdviceImage.swift
@@ -9,14 +9,16 @@ import SwiftUI
 
 struct HealthAdviceImage: View {
     var body: some View {
-        Image("TuVan").frame(width: 67, height: 67)
-        Text("Tư vấn sức khoẻ")
-            .font(
-            Font.custom("SF Pro Text", size: 12)
-            .weight(.semibold)
-            )
-            .multilineTextAlignment(.center)
-            .foregroundColor(.white)
+        VStack {
+            Image("TuVan").frame(width: 67, height: 67)
+            Text("Tư vấn sức khoẻ")
+                .font(
+                Font.custom("SF Pro Text", size: 12)
+                .weight(.semibold)
+                )
+                .multilineTextAlignment(.center)
+                .foregroundColor(.white)
+        }
     }
 }
 
